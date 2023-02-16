e0a9f4c6871defc43f81d7786e9562f9a1158b69
Santhosh <santhoshhariharan1393@gmail.com>
2023-02-16 23:43:46 +0530

    waste commit

---
+++ phone.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://gatewayapi.com/rest/mtsms"
+api_token = "YOUR_API_TOKEN"
+sender_id = "YOUR_SENDER_ID"
+message = "Hello, world!"
+recipients = ["1234567890", "0987654321"]
+
+headers = {
+    "Content-Type": "application/json",
+    "X-Gateway-APIKey": api_token
+}
+
+data = {
+    "sender": sender_id,
+    "message": message,
+    "recipients": recipients
+}
+
+response = requests.post(url, headers=headers, json=data)
