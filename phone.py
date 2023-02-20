0e6aa51f1027fc17b3e51d02afd688031c6b89f6
Santhosh <santhoshhariharan1393@gmail.com>
2023-02-20 17:29:51 +0530

    Update phone.py

--- phone.py
+++ phone.py
@@ -6,10 +6,6 @@ sender_id = "YOUR_SENDER_ID"
 message = "Hello, world!"
 recipients = ["1234567890", "0987654321"]
 
-headers = {
-    "Content-Type": "application/json",
-    "X-Gateway-APIKey": api_token
-}
 
 
 response = requests.post(url, headers=headers, json=data)
